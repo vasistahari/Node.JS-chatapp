d4385dcc90437b788bb531b53546f5d86299db90
dev hari <vasistahari52@gmail.com>
2018-06-27 20:54:57 +0530

    setup

---
+++ index.js
@@ -0,0 +1,39 @@
+var express=require('express');
+
+var socket=require("socket.io");
+var app=express();
+
+
+const path=require('path');
+const publicPath=path.join(__dirname,'/../public');
+const PORT=process.env.PORT || 3000;
+
+
+var server=app.listen(PORT,function(){
+    console.log("the server is listening on the port "+PORT);
+});
+
+app.use(express.static('public'));
+
+
+var io=socket(server);
+io.on('connection',function(socket){
+
+socket.on("chat",function(data){
+
+    io.sockets.emit("chat",data);
+
+});
+
+socket.on("typing",function(data){
+socket.broadcast.emit("typing",data)
+
+});
+
+
+
+
+console.log(socket.id);
+
+});
+
---
+++ public/chat.js
@@ -0,0 +1,34 @@
+var socket=io.connect("http://localhost:3000");
+
+var message= document.getElementById("message");
+var Name= document.getElementById("Name");
+var btn= document.getElementById("send");
+var output= document.getElementById("output");
+var feedback= document.getElementById("feedback");
+
+btn.addEventListener("click",function(){
+
+socket.emit("chat",{message:message.value,Name:Name.value});
+
+});
+
+
+
+
+message.addEventListener("keypress",function(){
+    socket.emit("typing",Name.value);
+});
+
+
+
+socket.on("chat",function(data){
+
+feedback.innerHTML="";
+output.innerHTML +='<p><strong><u>'+data.Name +'</u>: </strong>'+data.message+'</p>'
+});
+
+socket.on("typing",function(data){
+
+feedback.innerHTML='<p><em>' + data + ' is typing a message...</em></p>';
+
+});
