16e8fffef3a87ec9899f5e41792a173c85892701
Dev Hari <vasistahari52@gmail.com>
2018-07-13 18:27:57 +0530

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-var socket=io.connect("http://localhost:5000");
+var socket=io.connect("https://sendmsgs.herokuapp.com:5000");
 
 var message= document.getElementById("message");
 var Name= document.getElementById("Name");
